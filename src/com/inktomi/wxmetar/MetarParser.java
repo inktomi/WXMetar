c6e82759af9f4e09e84ede7fe4863daedec4c81d
Matthew Runo <matthew@Cujo.local>
2011-03-04 22:20:02 -0800

    First commit

---
+++ src/com/inktomi/wxmetar/MetarParser.java
@@ -0,0 +1,9 @@
+package com.inktomi.wxmetar;
+
+import com.inktomi.wxmetar.metar.Metar;
+
+public class MetarParser {
+    public static Metar parserMetar(final String input){
+
+    }
+}
---
+++ src/com/inktomi/wxmetar/metar/Metar.java
@@ -0,0 +1,11 @@
+package com.inktomi.wxmetar.metar;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: matthew
+ * Date: 3/4/11
+ * Time: 10:18 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Metar {
+}
